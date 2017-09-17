e5715faacbec4360865774a7ea7d62f78df00ea2
DataD00d <chris.peterson508@gmail.com>
2017-09-16 20:33:53 -0500

    Add files via upload

---
+++ Plot 1 Code.R
@@ -0,0 +1,19 @@
+#Set Data
+data <- read.table("C:/Users/Chris/Desktop/Coursera/Exploratory Data Analysis/household_power_consumption.txt", header = T, sep = ";")
+data2 <- subset(data, data$Date %in% c("1/2/2007", "2/2/2007"))
+rm(data)
+
+#format data
+data2$Global_active_power <- as.numeric(as.character(data2$Global_active_power))
+data2$Global_reactive_power <- as.numeric(as.character(data2$Global_reactive_power))
+data2$Sub_metering_1 <- as.numeric(as.character(data2$Sub_metering_1))
+data2$Sub_metering_2 <- as.numeric(as.character(data2$Sub_metering_2))
+data2$Voltage <- as.numeric(as.character(data2$Voltage))
+data2$Date <- as.Date(data2$Date, format = "%d/%m/%Y")
+data2$DateTime <- paste(data2$Date, data2$Time)
+data2$DateTime <- as.POSIXct(data2$DateTime)
+
+#Plot 1
+hist(data2$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.copy(png, 'C:/Users/Chris/Desktop/Coursera/Exploratory Data Analysis/Plot 1.png')
+dev.off()
